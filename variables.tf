b1efc9e3880aecc7c0b2c9948b23634fe7b8d437
chaitanyakumarmelam <melamchaitanyakumar@gmail.com>
2023-07-18 04:34:14 +0530

    Updating Terraform Config files

--- variables.tf
+++ variables.tf
@@ -15,11 +15,11 @@ variable "instance-type" {
 }
 
 variable "vpc-cidr" {
-  default = "10.10.0.0/16"
+  default = "10.0.0.0/16"
 }
 
 variable "subnet1-cidr" {
-  default = "10.10.1.0/24"
+  default = "10.0.1.0/24"
 
 }
 variable "subent_az" {
